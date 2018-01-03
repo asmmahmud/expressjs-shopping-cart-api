5c2ec0b7eacecfb006b53634f9eb9b28fa003b00
asmmahmud <asmmahmud@gmail.com>
2018-01-03 18:25:24 +0600

    products-list

---
+++ mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Product = require('./Product');
+const products = require('./products');
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect('mongodb://localhost/shopping-cart-api', {
+    useMongoClient: true
+  })
+  .then(() => {
+    console.log('Mongodb is connected!!');
+  })
+  .catch(err => {
+    console.warn(err);
+  });
+  
+  const productPromises = products.map(prod=>{
+	  const productData = {
+		name: prod.name,
+		category: prod.category,
+		brand: prod.brand,
+		price: prod.price,
+		quantity: prod.quantity
+	  };
+	  if (prod.model) {
+		productData.model = prod.model;
+	  }
+	  const product = new Product(productData);
+	  return product.save();
+		
+  });
+
+	Promise.all(productPromises)
+	.then(savedProducts => {
+		console.log(savedProducts.length + ' products have been saved!');
+	}).catch(console.log);
+
+ 
---
+++ products.js
@@ -0,0 +1,298 @@
+module.exports = [
+{   "name": "Apple iPhone X",
+    "category": "Cell Phone",
+    "brand": "Apple",
+    "model": "iPhone X",
+    "price": 999,
+    "quantity": 14
+},
+{
+	"name": " Apple iPhone 8",
+    "category": "Cell Phone",
+    "brand": "Apple",
+    "model": "iPhone 8",
+    "price": 849,
+    "quantity": 10
+},
+{
+	"name": "Google Pixel 2 XL",
+    "category": "Cell Phone",
+    "brand": "Google",
+    "model": "XL",
+    "price": 649.99,
+    "quantity": 10
+},
+{
+	"name": "Google Pixel XL",
+    "category": "Cell Phone",
+    "brand": "Google",
+    "model": "Pixel XL",
+    "price": 414.99,
+    "quantity": 11
+},
+{
+	"name": "HTC U11",
+    "category": "Cell Phone",
+    "brand": "HTC",
+    "model": "U11",
+    "price": 572.97,
+    "quantity": 11
+},
+{
+	"name": "LG V30",
+    "category": "Cell Phone",
+    "brand": "LG",
+    "model": "V30",
+    "price": 800,
+    "quantity": 9
+},
+{
+	"name": "LG G6",
+    "category": "Cell Phone",
+    "brand": "LG",
+    "model": "G6",
+    "price": 518,
+    "quantity": 15
+},
+{
+	"name": "Motorola Moto Z2",
+    "category": "Cell Phone",
+    "brand": "Motorola",
+    "model": "Moto Z2",
+    "price": 399,
+    "quantity": 14
+},
+{
+	"name": "Motorola Moto G5 Plus",
+    "category": "Cell Phone",
+    "brand": "Motorola",
+    "model": "Moto G5 Plus",
+    "price": 184.99,
+    "quantity": 23
+},
+{
+	"name": "Motorola Moto E4",
+    "category": "Cell Phone",
+    "brand": "Motorola",
+    "model": "Moto E4",
+    "price": 129.99,
+    "quantity": 11
+},
+{
+	"name": "OnePlus 5T",
+    "category": "Cell Phone",
+    "brand": "OnePlus",
+    "model": "Midnight Black",
+    "price": 499,
+    "quantity": 5
+},
+{
+	"name": "Samsung Galaxy S8",
+    "category": "Cell Phone",
+    "brand": "Samsung",
+    "model": "S8",
+    "price": 849.99,
+    "quantity": 10
+},
+{
+	"name": "Samsung Galaxy Note 8",
+    "category": "Cell Phone",
+    "brand": "Samsung",
+    "model": "Note 8",
+    "price": 896.54,
+    "quantity": 10
+},
+{
+	"name": "Samsung Galaxy S7",
+    "category": "Cell Phone",
+    "brand": "Samsung",
+    "model": "Galaxy S7",
+    "price": 448,
+    "quantity": 17
+},
+{
+	"name": "Sony Xperia XZ1 Compact",
+    "category": "Cell Phone",
+    "brand": "Sony",
+    "model": "XZ1 Compact",
+    "price": 599.99,
+    "quantity": 13
+},
+{
+	"name": "Sony Xperia R1 (Plus)",
+    "category": "Cell Phone",
+    "brand": "Sony",
+    "model": "R1 (Plus)",
+    "price": 218.68,
+    "quantity": 11
+},
+{
+	"name": "ZTE Blade Z Max",
+    "category": "Cell Phone",
+    "brand": "ZTE",
+    "model": "Z Max",
+    "price": 129,
+    "quantity": 11
+},
+{
+	"name": "Dell XPS 13",
+	"category": "Laptop",
+	"brand": "Dell",
+	"price": 1151.16,
+	"quantity": 7
+},
+{
+	"name": "Asus Zenbook UX310UA",
+	"category": "Laptop",
+	"brand": "Asus",
+	"price": 830,
+	"quantity": 10
+},
+{
+	"name": "Lenovo Yoga Book",
+	"category": "Laptop",
+	"brand": "Lenovo",
+	"price": 252.99,
+	"quantity": 15
+},
+{
+	"name": "Apple Macbook Pro with Touch Bar",
+	"category": "Laptop",
+	"brand": "Apple",
+	"price": 2199,
+	"quantity": 10
+},
+{
+	"name": "Asus ROG Zephyrus GX501",
+	"category": "Laptop",
+	"brand": "Asus",
+	"price": 2099,
+	"quantity": 10
+},
+{
+	"name": "Dell Inspiron 15 7000 Gaming",
+	"category": "Laptop",
+	"brand": "Dell",
+	"price": 680,
+	"quantity": 10
+},
+{
+	"name": "Asus Chromebook Flip",
+	"category": "Laptop",
+	"brand": "Asus",
+	"price": 609.99,
+	"quantity": 10
+},
+{
+	"name": "Microsoft Surface Book 2",
+	"category": "Laptop",
+	"brand": "Microsoft",
+	"price": 1268,
+	"quantity": 10
+},
+{
+	"name": "Acer Switch 3",
+	"category": "Laptop",
+	"brand": "Acer",
+	"price": 439,
+	"quantity": 10
+},
+{
+	"name": "Apple MacBook 12-inch",
+	"category": "Laptop",
+	"brand": "Apple",
+	"price": 1279,
+	"quantity": 12
+},
+{
+	"name": "Google Pixelbook",
+	"category": "Laptop",
+	"brand": "Google",
+	"price": 890,
+	"quantity": 12
+},
+{
+	"name": "BenQ PD3200U",
+	"category": "Monitor",
+	"brand": "BenQ",
+	"price": 799,
+	"quantity": 12
+},
+{
+	"name": "AOC Agon AG352UCG",
+	"category": "Monitor",
+	"brand": "AOC",
+	"price": 849.99,
+	"quantity": 12
+},
+{
+	"name": "Acer Predator X34",
+	"category": "Monitor",
+	"brand": "Acer",
+	"price": 844.99,
+	"quantity": 12
+},
+{
+	"name": "Volkswagen e-Golf hatchback",
+	"category": "Electric Car",
+	"brand": "Volkswagen",
+	"price": 32730,
+	"quantity": 12
+},
+{
+	"name": "Hyundai Ioniq Electric",
+	"category": "Electric Car",
+	"brand": "Volkswagen",
+	"price": 31295,
+	"quantity": 12
+},
+{
+	"name": "Tesla Model S hatchback",
+	"category": "Electric Car",
+	"brand": "Tesla",
+	"price": 127135,
+	"quantity": 12
+},
+{
+	"name": "Renault ZOE hatchback",
+	"category": "Electric Car",
+	"brand": "Renault",
+	"price": 30520,
+	"quantity": 12
+},
+{
+	"name": "Volkswagen e-up! hatchback",
+	"category": "Electric Car",
+	"brand": "Volkswagen",
+	"price": 25640,
+	"quantity": 12
+},
+{
+	"name": "Nissan Leaf hatchback",
+	"category": "Electric Car",
+	"brand": "Nissan",
+	"price": 30990,
+	"quantity": 12
+},
+{
+	"name": "BMW i3 hatchback",
+	"category": "Electric Car",
+	"brand": "BMW",
+	"price": 40125,
+	"quantity": 12
+},
+{
+	"name": "Kia Soul EV hatchback",
+	"category": "Electric Car",
+	"brand": "Kia Soul EV",
+	"price": 30495,
+	"quantity": 12
+},
+{
+	"name": "Tesla Model X SUV",
+	"category": "Electric Car",
+	"brand": "Tesla",
+	"price": 133185,
+	"quantity": 12
+}
+];
