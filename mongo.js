f0cb7a360e451ad3dd193d88d13a43fb324f1c21
asmmahmud <asmmahmud@gmail.com>
2018-01-04 15:24:51 +0600

    update-4-1-18-1

--- mongo.js
+++ mongo.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const Product = require('./Product');
 const products = require('./products');
+const config = require('./config/index');
 
 mongoose.Promise = global.Promise;
 mongoose
-  .connect('mongodb://localhost/shopping-cart-api', {
+  .connect(config.mongoUri, {
     useMongoClient: true
   })
   .then(() => {
@@ -13,26 +14,24 @@ mongoose
   .catch(err => {
     console.warn(err);
   });
-  
-  const productPromises = products.map(prod=>{
-	  const productData = {
-		name: prod.name,
-		category: prod.category,
-		brand: prod.brand,
-		price: prod.price,
-		quantity: prod.quantity
-	  };
-	  if (prod.model) {
-		productData.model = prod.model;
-	  }
-	  const product = new Product(productData);
-	  return product.save();
-		
-  });
 
-	Promise.all(productPromises)
-	.then(savedProducts => {
-		console.log(savedProducts.length + ' products have been saved!');
-	}).catch(console.log);
+const productPromises = products.map(prod => {
+  const productData = {
+    name: prod.name,
+    category: prod.category,
+    brand: prod.brand,
+    price: prod.price,
+    quantity: prod.quantity
+  };
+  if (prod.model) {
+    productData.model = prod.model;
+  }
+  const product = new Product(productData);
+  return product.save();
+});
 
- 
+Promise.all(productPromises)
+  .then(savedProducts => {
+    console.log(savedProducts.length + ' products have been saved!');
+  })
+  .catch(console.log);
